8edecb40ae616accd0ea0cf24b9fff6b1a3c0663
Pedro Henrique <pedrohenrique.phas16@gmail.com>
2025-10-07 00:34:46 +0000

    conexao com o banco em andamento

---
+++ tarefas/Operacoes.cs
@@ -0,0 +1,47 @@
+public class Operacoes
+{
+
+    private string connectionString = "";
+
+    public int Criar(Tarefa tarefa)
+    {
+        using(var conexao = new MySqlConnection(connectionString)){
+            conexao.Open();
+            string sql = @"INSERT INTO tarefa (nome, descricao, dataCriacao, status, dataExecucao)
+                        VALUES(@nome, @descricao, @dataCriacao, @status, @dataExecucao)
+                        SELECT LAST_INSERT_ID();";
+            using (var cmd = new MySqlConnection(sql, conexao)){
+                cmd.Parameters.AddWithValue("@nome", tarefa.Nome);
+                cmd.Parameters.AddWithValue("@descricao", tarefa.Descricao);
+                cmd.Parameters.AddWithValue("@dataCriacao", tarefa.DataCriacao);
+                cmd.Parameters.AddWithValue("@status", tarefa.Status);
+                cmd.Parameters.AddWithValue("@dataExecucao", tarefa.DataExecucao);
+
+                return Convert.ToInt32(cmd.ExecuteScalar());
+            }            
+        }
+        return 0;
+    }
+
+    public Tarefa Buscar(int id)
+    {
+        return null;
+    }
+
+    public List<Tarefa> Listar()
+    {
+        return Array.Empty<Tarefa>();
+    }
+
+    public void Alterar(Tarefa tarefa)
+    {
+
+    }
+
+    public void Excluir(int id)
+    {
+        
+    }
+
+
+}
